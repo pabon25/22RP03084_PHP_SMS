91a922b0833f461ad7e5e0538bdfa6df15226f6e
pabon25 <139360532+pabon25@users.noreply.github.com>
2024-08-06 20:37:34 +0200

    index.php

--- index.php
+++ index.php
@@ -17,8 +17,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             if ($rememberMe) {
                 setcookie('username', $username, time() + (86400 * 60), "/"); 
+                setcookie('password', $password, time() + (86400 * 60), "/"); 
             } else {
-                setcookie('username', '', time() - 3600, "/");
+                setcookie('caches', '', time() - 3600, "/");
+                setcookie('caches', '', time() - 3600, "/");
+
             }
 
             $_SESSION['user_id'] = $user['id'];
@@ -33,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
-$storedUsername = isset($_COOKIE['username']) ? htmlspecialchars($_COOKIE['username']) : '';
+$storedUsername = isset($_COOKIE['caches']) ? htmlspecialchars($_COOKIE['caches']) : '';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -55,11 +58,11 @@ $storedUsername = isset($_COOKIE['username']) ? htmlspecialchars($_COOKIE['usern
                 <p class="error"><?php echo htmlspecialchars($error); ?></p>
             <?php endif; ?>
             <label for="uname">Username:</label>
-            <input type="text" id="uname" name="uname" value="<?php echo $storedUsername; ?>" required><br>
+            <input type="text" id="uname" name="uname" value="<?php echo isset($_COOKIE['caches']) ? $_COOKIE['caches'] : ""  ?>" required><br>
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br>
+            <input type="password" id="password" name="password" required value="<?php echo isset($_COOKIE['password']) ? $_COOKIE['password'] : ""  ?>"><br>
             <label for="remember">
-                <input type="checkbox" id="remember" name="remember" <?php echo isset($_COOKIE['username']) ? 'checked' : ''; ?>>
+                <input type="checkbox" id="remember" name="remember" <?php echo isset($_COOKIE['caches']) ? 'checked' : ''; ?>>
                 Remember me
             </label><br>
             <button type="submit">Login</button>
